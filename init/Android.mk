cf10922e61848a37efef0b7b1cc82b2247602cb2
Hardslog <hardslog@gmail.com>
2017-02-08 16:33:25 -0500

    Bluetooth works with this commit and gerrit number 21740

---
+++ init/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_CFLAGS := -Wall -O3
+LOCAL_SRC_FILES := init_oneplus2.cpp
+LOCAL_MODULE := libinit_oneplus2
+
+ifeq ($(TARGET_DEVICE),oneplus2)
+  include $(BUILD_STATIC_LIBRARY)
+endif
