26bf8d6c8093053467a2d9e060bb6bf1b1eab773
hardslog2 <hardslog@gmail.com>
2017-02-14 17:17:02 -0500

    major cleanup and D2TW
    Activate Double tap to wake
    Fix the derp that didn't bring in APNs
    cleanup other stuff from other roms that were brought in by forking

--- omni_oneplus2.mk
+++ omni_oneplus2.mk
@@ -17,10 +17,12 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 
 # Get the prebuilt list of APNs
-$(call inheret-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # must be before including omni part
 TARGET_BOOTANIMATION_SIZE := 1080x608
