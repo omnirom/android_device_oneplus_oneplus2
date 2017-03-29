6aba574ce85287d206f5aa30505d53d3b4571dae
maxwen <max.weninger@gmail.com>
2017-03-29 23:19:28 +0200

    Oneplus3Parts: simplify key event handling
    Rework alert slider event handling
    
    Note: we really want to handle DT2W in Keyhandler and not
    via WAKEUP in keylayout cause this way we can use the proxi
    check feature for it
    
    Change-Id: I6a78584f219cc0afbff07190311245b64d65eb65

--- Oneplus2Parts/src/org/omnirom/device/KeyHandler.java
+++ Oneplus2Parts/src/org/omnirom/device/KeyHandler.java
@@ -262,21 +262,6 @@ public class KeyHandler implements DeviceKeyHandler {
                 dispatchMediaKeyWithWakeLockToAudioService(KeyEvent.KEYCODE_MEDIA_NEXT);
             }
             break;
-        case KEY_SLIDER_TOP:
-            if (DEBUG) Log.i(TAG, "KEY_SLIDER_TOP");
-            mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
-            doHandleSliderAction(0);
-            break;
-        case KEY_SLIDER_CENTER:
-            if (DEBUG) Log.i(TAG, "KEY_SLIDER_CENTER");
-            mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
-            doHandleSliderAction(1);
-            break;
-        case KEY_SLIDER_BOTTOM:
-            if (DEBUG) Log.i(TAG, "KEY_SLIDER_BOTTOM");
-            mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
-            doHandleSliderAction(2);
-            break;
         }
     }
 
@@ -289,6 +274,21 @@ public class KeyHandler implements DeviceKeyHandler {
         boolean isKeySupported = ArrayUtils.contains(sHandledGestures, event.getScanCode());
         if (isKeySupported) {
             if (DEBUG) Log.i(TAG, "scanCode=" + event.getScanCode());
+            switch(event.getScanCode()) {
+                case KEY_SLIDER_TOP:
+                    if (DEBUG) Log.i(TAG, "KEY_SLIDER_TOP");
+                    doHandleSliderAction(0);
+                    return true;
+                case KEY_SLIDER_CENTER:
+                    if (DEBUG) Log.i(TAG, "KEY_SLIDER_CENTER");
+                    doHandleSliderAction(1);
+                    return true;
+                case KEY_SLIDER_BOTTOM:
+                    if (DEBUG) Log.i(TAG, "KEY_SLIDER_BOTTOM");
+                    doHandleSliderAction(2);
+                    return true;
+            }
+
             Message msg = getMessageForKeyEvent(event);
             mEventHandler.removeMessages(GESTURE_REQUEST);
             mEventHandler.sendMessage(msg);
@@ -434,23 +434,20 @@ public class KeyHandler implements DeviceKeyHandler {
     private void doHandleSliderAction(int position) {
         int action = getSliderAction(position);
         if ( action == 0) {
-            mNoMan.setZenMode(Global.ZEN_MODE_OFF, null, TAG);
+            mNoMan.setZenMode(Global.ZEN_MODE_OFF_ONLY, null, TAG);
             mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
         } else if (action == 1) {
-            mNoMan.setZenMode(Global.ZEN_MODE_OFF, null, TAG);
+            mNoMan.setZenMode(Global.ZEN_MODE_OFF_ONLY, null, TAG);
             mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
         } else if (action == 2) {
             mNoMan.setZenMode(Global.ZEN_MODE_OFF_ONLY, null, TAG);
             mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
         } else if (action == 3) {
             mNoMan.setZenMode(Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS, null, TAG);
-            mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
         } else if (action == 4) {
             mNoMan.setZenMode(Global.ZEN_MODE_ALARMS, null, TAG);
-            mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
         } else if (action == 5) {
             mNoMan.setZenMode(Global.ZEN_MODE_NO_INTERRUPTIONS, null, TAG);
-            mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
         }
     }
 }
